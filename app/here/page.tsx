3d12422af5555a2a3d7048d7a8cf216705ee3431
duy <khanhduy@lottecnf.com>
2025-12-11 12:17:37 +0700

    init

---
+++ app/here/page.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+
+export default function Here (props: IHereProps) {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+        <p className='text-6xl'>Why you still click here?</p>
+    </div>
+  );
+}
